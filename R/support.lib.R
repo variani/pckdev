de38ac74aaaa4d40286fe8038cf8a28fcafd73e2
variani <machovariani@gmail.com>
2013-06-11 17:57:06 +0200

    added first R scripts

---
+++ R/support.lib.R
@@ -0,0 +1,13 @@
+#' Function that returns list of available methods.
+#'
+#' @name availableMethods
+#' @param A Dummy parameter.
+#' @param B Dummy parameter.
+#' @return List of available methods.
+#' @examples
+#'  availableMethods()
+#' @export
+availableMethods <- function(A, B)
+{
+  c("sqrt")
+}
