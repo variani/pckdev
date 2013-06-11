2f53f954d1967fef1a6a491ee5d5b2123d35e77f
variani <machovariani@gmail.com>
2013-06-11 17:12:38 +0200

    added first files of R package skeleton

---
+++ R/package.R
@@ -0,0 +1,13 @@
+#' Package pckdev.
+#'
+#' Skeleton of R package.
+#' @name pckdev
+#' @rdname pckdev
+#' @docType package
+#' @examples
+#` s <- Sensor()
+#` s
+#' @import methods
+#' @import graphics
+{}
+
